ec8f59b4638e9e40264e3032891b60ab5c4ba508
hellencristinafernandesdelima <hellencristinafernandesdelima@gmail.com>
2020-03-23 22:16:58 -0300

    alterações

--- Receitas_Senado/Projeto_Talend/RECEITAS_SENADO/poms/jobs/process/job_extracao_receitas_senadores_0.1/src/main/templates/Job_run_template.ps1
+++
@@ -1,3 +0,0 @@
-$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-cd $fileDir
-java '-Dtalend.component.manager.m2.repository=%cd%/../lib' '-Xms256M' '-Xmx1024M' '-Dfile.encoding=UTF-8' -cp '.;../lib/routines.jar;../lib/crypto-utils.jar;../lib/dom4j-1.6.1.jar;../lib/log4j-1.2.17.jar;../lib/mysql-connector-java-8.0.12.jar;job_extracao_receitas_senadores_0_1.jar;' receitas_senado.job_extracao_receitas_senadores_0_1.job_extracao_receitas_senadores  --context=Default %*
